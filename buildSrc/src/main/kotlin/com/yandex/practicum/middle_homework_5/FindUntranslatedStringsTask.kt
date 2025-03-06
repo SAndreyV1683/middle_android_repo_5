876a5d4353e3dfa5f545e5cf3e9fd619c6a68148
Sboev Andrey <a.sboev@teleswyz.ru>
2025-03-06 18:59:04 +0300

    find untranslated strings in other files

--- buildSrc/src/main/kotlin/com/yandex/practicum/middle_homework_5/FindUntranslatedStringsTask.kt
+++ buildSrc/src/main/kotlin/com/yandex/practicum/middle_homework_5/FindUntranslatedStringsTask.kt
@@ -10,26 +10,47 @@ abstract class FindUntranslatedStringsTask : DefaultTask() {
     @TaskAction
     fun findUntranslatedStrings() {
         val resDir = File(project.projectDir, "src/main/res")
-        val strings = File(resDir, "values/strings.xml")
-        val stringsFromXml = DocumentBuilderFactory
+        val defaultStrings = File(resDir, "values/strings.xml")
+        val defaultStringsFromXml = DocumentBuilderFactory
             .newInstance()
             .newDocumentBuilder()
-            .parse(strings)
+            .parse(defaultStrings)
             .getElementsByTagName("string")
-        val stringIdentities = stringsFromXml.let { nodeList ->
+        val defaultStringIdentities = defaultStringsFromXml.let { nodeList ->
             (0 until nodeList.length).map { i ->
                 val node = nodeList.item(i)
                 val name = node.attributes?.getNamedItem("name")?.nodeValue ?: ""
                 name
             }
         }
-        if (stringIdentities.isNotEmpty()) {
+        val fileName = "strings.xml"
+        val foldersStartingWithValues = resDir.listFiles { file ->
+            file.isDirectory && file.name.startsWith("values-")
+                    && file.listFiles()?.any { it.isFile && it.name == fileName } ?: false
+        }
+        foldersStartingWithValues?.let { array ->
             val stringBuilderErrorText = StringBuilder("Missing translations").append(System.lineSeparator())
-            stringIdentities.forEach { missing ->
-                stringBuilderErrorText
-                    .append("=== $missing ===")
-                    .append(System.lineSeparator())
-                    .append(System.lineSeparator())
+            for (folder in array) {
+                val strings = File(resDir, "${folder.name}/strings.xml")
+                val stringsFromXml = DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .parse(strings)
+                    .getElementsByTagName("string")
+                val stringIdentities = stringsFromXml.let { nodeList ->
+                    (0 until nodeList.length).map { i ->
+                        val node = nodeList.item(i)
+                        val name = node.attributes?.getNamedItem("name")?.nodeValue ?: ""
+                        name
+                    }
+                }
+                defaultStringIdentities.forEach { missing ->
+                    if (!stringIdentities.contains(missing)) {
+                        stringBuilderErrorText
+                            .append("=== $missing in ${folder.name} ===")
+                            .append(System.lineSeparator())
+                    }
+                }
             }
             throw GradleException(stringBuilderErrorText.toString())
         }
